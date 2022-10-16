87451431411518e204780aa687e07a5cf73e3aa3
Idris Aminat <109235073+Aminat133@users.noreply.github.com>
2022-10-16 22:12:34 +0100

    Update 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -7,7 +7,7 @@
 /**
  *main - Entry Point
  *
- *return: always 0 (success)
+ *Return: Always 0 (success)
  */
 
 int main(void)
@@ -23,7 +23,7 @@ printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
 }
 else if (n % 10 == 0)                                                                                                        
 {                                                                                                                
-printf("Last digit of %d is %d and is gand is 0\n", n, n % 10);                                           
+printf("Last digit of %d is %d and is 0\n", n, n % 10);                                           
 }
 else if (n % 10 < 0 && n% 10 != 0)                                                                                                        
 {                                                                                                                
