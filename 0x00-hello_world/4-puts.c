3a8237b3d7d9b8bde5e19aadf940bf97ef9e7c95
Aminat Idris <idris.oaminat@gmail.com>
2022-10-16 06:59:21 -0700

    add files 1 2 3 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int puts(void)
+{
+strlen("\"Programming is like building a multilingual puzzle\n"));
+return (0);
+}
