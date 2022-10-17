c6db328f6c2ec9a134d2f40f2829ca7434ea2398
Idris Aminat <109235073+Aminat133@users.noreply.github.com>
2022-10-17 18:25:45 +0100

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,7 +12,7 @@ int digit1, digit2;
 
 for (digit1 = 0; digit1 < 10; digit1++)
 {
-for (digit2 = 0; digit2 < 10; digit2++)
+for (digit2 = 1; digit2 < 10; digit2++)
 {
 putchar((digit1 % 10) + '0');
 putchar((digit2 % 10) + '0');
