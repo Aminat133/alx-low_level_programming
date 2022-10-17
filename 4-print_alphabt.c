1fdcde9c4a5e54084ef1bee45354a13cb5757ad4
Aminat Idris <idris.oaminat@gmail.com>
2022-10-17 04:15:05 -0700

    added file 4-print_alphabt.c

---
+++ 4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ *Return: Always 0 (success)
+ */
+
+int main(void)
+{
+
+char a = 97;
+while (a < 123 && a != 101 && a != 113)
+    {
+      putchar(a);
+      a++;
+    }
+  putchar('\n');
+
+  return (0);
+}
