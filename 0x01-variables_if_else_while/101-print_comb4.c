49b25b7ddd7badcb60c69a8fa75c68f8d7faca86
Idris Aminat <109235073+Aminat133@users.noreply.github.com>
2022-10-17 19:20:12 +0100

    Update 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -22,14 +22,14 @@ putchar((digit3 % 10) + '0');
 
 if (digit1 == 7 && digit2 == 8 && digit3 == 9)
 continue;
-	      
+
 putchar(',');
 putchar(' ');
 }
 }
 }
 
-  putchar('\n');
+putchar('\n');
 
-  return (0);
+return (0);
 }
