57ee718280d832142fb8154066d80ea3fa42b777
Idris Aminat <109235073+Aminat133@users.noreply.github.com>
2022-10-17 12:10:53 +0100

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,20 +9,21 @@
 int main(void)
 {
 
-  char a = 97;
-  while (a < 123)
-    {
-      putchar(a);
-      a++;
+char a = 97;
+  
+while (a < 123)
+{
+putchar(a);
+a++;
 }
 a = 65;
- 
+
 while (a < 91)
 {
 putchar(a);
 a++;
 }
- 
+
 putchar('\n');
 
 return (0);
