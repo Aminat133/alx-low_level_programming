f3101225ef6d6b43b510e997b595d879acbfc6d2
Idris Aminat <109235073+Aminat133@users.noreply.github.com>
2022-10-17 19:55:46 +0100

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,28 +8,36 @@
 
 int main(void)
 {
-  int digit1, digit2;
+int digit1, digit2;
 
-  for (digit1 = 0; digit1 < 9; digit1++)
-    {
-      for (digit2 = 1; digit2 < 10; digit2++)
-	{
+for (digit1 = 0; digit1 < 10; digit1++)
+{
+for (digit2 = 1; digit2 < 10; digit2++)
+{
+for (digit3 = 0; digit3 < 10; digit3++)
+{
+for (digit4 = 0; digit4 < 10; digit4++)
+{
 
-	  if (digit2 <= digit1)
-	    continue;
+if (digit1 == digit3 && digit2 == digit4)
+continue;
 
-	  putchar((digit1 % 10) + '0');
-	  putchar((digit2 % 10) + '0');
+putchar((digit1 % 10) + '0');
+putchar((digit2 % 10) + '0');
 
-	  if (digit1 == 8 && digit2 == 9)
-	    continue;
+putchar(' ');
+putchar((digit1 % 10) + '0');
+putchar((digit2 % 10) + '0');
 
-	  putchar(',');
-	  putchar(' ');
-	}
-    }
+if (digit1 == 9 && digit2 == 8 && digit3 == 9 && digit4 == 9)
+continue;
 
-  putchar('\n');
+putchar(',');
+putchar(' ');
+}
+}
 
-  return (0);
+putchar('\n');
+
+return (0);
 }
