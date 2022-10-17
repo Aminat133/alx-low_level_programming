f5bfb08745a5e1b9837623a84f6fb0075a97be49
Idris Aminat <109235073+Aminat133@users.noreply.github.com>
2022-10-17 13:33:48 +0100

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-char a = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
+char a = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 fprintf(stderr, a);
 return (1);
 }
