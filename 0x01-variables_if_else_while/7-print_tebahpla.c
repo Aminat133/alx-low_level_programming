2078136b62e5433bb1a852e1d847541a7edafc61
Aminat Idris <idris.oaminat@gmail.com>
2022-10-17 06:20:52 -0700

    add files 8-print_base16.c 7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ *Return: Always 0 (success)
+ */
+
+int main(void)
+{
+
+char a = 122;
+while (a > 96)
+{
+putchar(a);
+a--;
+}
+putchar('\n');
+
+return (0);
+}
---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ *Return: Always 0 (success)
+ */
+
+int main(void)
+{
+
+int a = 0;
+while (a < 10)
+{
+putchar(a + '0');
+a++;
+}
+char b = 97;
+while (b < 103)
+{
+putchar(b);
+b++;
+}
+putchar('\n');
+
+return (0);
+}
