a4cb5f1012cff4433ce652945c8d4cb95a3d7ef9
Aminat Idris <idris.oaminat@gmail.com>
2022-10-17 04:38:11 -0700

    add file 6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ *Return: Always 0 (success)
+ */
+
+int main(void)
+{
+
+int a = 0;
+while (a < 10)
+{
+putchar(a);
+a++;
+}
+putchar('\n');
+
+return (0);
+}
