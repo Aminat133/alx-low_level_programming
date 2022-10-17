96ad9b7c09d37edfdd26f78afd98cd1e29dfb88c
Aminat Idris <idris.oaminat@gmail.com>
2022-10-17 04:08:56 -0700

    added 3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ *Return: Always 0 (success)
+ */
+
+int main(void)
+{
+
+  char a = 97;
+  while (a < 123)
+    {
+      putchar(a);
+      a++;
+}
+a = 65;
+ 
+while (a < 91)
+{
+putchar(a);
+a++;
+}
+ 
+putchar('\n');
+
+return (0);
+}
