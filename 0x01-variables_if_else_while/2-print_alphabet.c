12ad94699c94c22bc5c7eb1e78bc722d5ad72f2d
Aminat Idris <idris.oaminat@gmail.com>
2022-10-17 00:56:36 -0700

    add file 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ *Return: Always 0 (success)
+
+int main(void)
+{
+
+int a = 97;
+while(a < 123)
+{
+putchar("%i\n", a);
+a++
+}
+
+return (0);
+}
