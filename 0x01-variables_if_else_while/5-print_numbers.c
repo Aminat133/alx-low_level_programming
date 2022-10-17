f105ec3661b10518a8f3bd6c874f78ba498e270d
Aminat Idris <idris.oaminat@gmail.com>
2022-10-17 04:32:26 -0700

    add file 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+nclude <stdio.h>
+
+/**
+ *  * main - Entry Point
+ *   *
+ *    *Return: Always 0 (success)
+ *     */
+
+int main(void)
+{
+int a = 0;
+while (a < 10)
+{
+printf("%i", a);
+a++;
+}
+putchar('\n');
+
+return (0);
+}
