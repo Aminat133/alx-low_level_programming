903c901aa2fd9bdb2f5349be2e29ee4118eff8a3
Aminat Idris <idris.oaminat@gmail.com>
2022-10-18 13:59:22 -0700

    add directory 0x02-functions_nested_loops and files

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ *  * main - Check description
+ *   * Description: It prints the word Holberton, followed by a new line.
+ *    * Return: Always 0 (success)
+ *     */
+int main(void)
+{
+		char word[8] = "_putchar";
+			int i;
+
+				for (i = 0; i < 9; i++)
+							_putchar(word[i]);
+					_putchar('\n');
+
+						return (0);
+}
