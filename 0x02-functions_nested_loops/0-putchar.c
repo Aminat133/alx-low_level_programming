b87cf935c2112805e918035f82c06f91f627a35d
Idris Aminat <109235073+Aminat133@users.noreply.github.com>
2022-10-19 22:14:11 +0100

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,7 +5,8 @@
  *  * main - Check description
  *   * Description: It prints the word Holberton, followed by a new line.
  *    * Return: Always 0 (success)
- *     */
+ *     
+ */
 int main(void)
 {
 char word[8] = "_putchar";
