91365278b8d081d8d057447b05d3d9f0a0e9bcce
Idris Aminat <109235073+Aminat133@users.noreply.github.com>
2022-10-19 22:11:27 +0100

    Update main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,1 +1,11 @@
-t
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+*main.h - Header file containing declarations for all functions
+*       used in the 0x02-functions_nested_loops directory.
+*/
+
+int _putchar(char c);
+
+#endif
