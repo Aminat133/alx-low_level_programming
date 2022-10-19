2b3cc2aaa4b9ba2ffa54c96065d7ba95e6400ce6
Idris Aminat <109235073+Aminat133@users.noreply.github.com>
2022-10-19 23:30:21 +0100

    Update 10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,8 +2,8 @@
 
 /**
  *  * add - Adds two integers.
- *   * @num1: The first integer to be added.
- *    * @num2: The second integer to be added.
+ *   * @no1: The first integer to be added.
+ *    * @no2: The second integer to be added.
  *     *
  *      * Return: The result of the addition.
  *
