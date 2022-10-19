cf186a9c3251c82326207e36149d491b1631141c
Idris Aminat <109235073+Aminat133@users.noreply.github.com>
2022-10-19 23:51:16 +0100

    Update 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,13 +11,13 @@ void print_to_98(int n)
 if (n > 98)
 {
 
-while (n >= 98)
+while (n > 98)
 {
 printf("%d, ", n);
 n--;
 }
 
-printf("\n");
+printf("%d\n", n);
 }
 
 else if (n == 98)
@@ -27,13 +27,13 @@ printf("%d\n", n);
 else
 {
 
-while (n <= 98)
+while (n < 98)
 {
 printf("%d, ", n);
 n++;
 }
 
-printf("\n");
+printf("%d\n", n);
 }
 }
 
