8d0fcba8d43c30505fcb23cac8241a8538229c26
Idris Aminat <109235073+Aminat133@users.noreply.github.com>
2022-10-22 00:11:54 +0100

    Create 3-print_remaining_days.c

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -12,7 +12,7 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-if ((year % 4 == 0 || year % 400 == 0) && (year % 100 != 0))
+if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0))
 {
 if (month >= 2 && day >= 60)
 {
@@ -22,6 +22,7 @@ day++;
 printf("Day of the year: %d\n", day);
 printf("Remaining days: %d\n", 366 - day);
 }
+
 else
 {
 if (month == 2 && day == 60)
