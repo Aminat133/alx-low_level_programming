6abd77b36016460b9c84fbd4a3cb09aef98f3ecd
Idris Aminat <109235073+Aminat133@users.noreply.github.com>
2022-10-22 00:45:59 +0100

    Update 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -4,7 +4,7 @@
  * _isupper - Checks for uppercase characters.
  * @c: The character to be checked.
  *
- * Return: 1 if character is uppercase, 
+ * Return: 1 if character is uppercase,
  *0 otherwise.
  */
 int _isupper(int c)
