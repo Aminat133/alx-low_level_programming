82396ef1b12c2c52c6b8c637b76fbb797b4cff57
Aminat Idris <idris.oaminat@gmail.com>
2023-01-10 13:08:59 -0800

    added dir 0x05-pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,25 @@
+nclude "main.h"
+
+#include <unistd.h>
+
+
+
+/**
+ *
+ *  *  *void reset_to_98- resets_to_98
+ *
+ *   *   * @n : The pointer variable
+ *
+ *    *    *
+ *
+ *     *     * Return: On success 0.
+ *
+ *      *       */
+
+void reset_to_98(int *n);
+
+{
+
+	*n = 98
+
+}
