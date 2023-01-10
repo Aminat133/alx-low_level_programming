b9832e50ef78e4938dbe3b846a9eeac473b29178
Aminat Idris <idris.oaminat@gmail.com>
2023-01-10 13:10:48 -0800

    added main.h file

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,18 @@
+fndef MAIN_H
+
+#define MAIN_H
+
+
+
+/**
+ *
+ * *main.h - Header file containing declarations for all functions
+ *
+ * *       used in the directory.
+ *
+ * */
+
+void reset_to_98(int *n);
+
+#endif
+
