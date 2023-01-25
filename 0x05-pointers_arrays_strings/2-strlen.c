5329092bee0ac05a9be8d0abd1ae5580300a45b9
Idris Aminat <109235073+Aminat133@users.noreply.github.com>
2023-01-25 21:55:27 +0100

    Update 2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,10 +1,11 @@
 #include "main.h"
 
 /**
-*_strlen -  Returns the length of a string.
-*@ *s - string
-*return: length of @s
-*/
+ * _strlen - Returns the length of a string.
+ * @s: The string to get the length of.
+ *
+ * Return: The length of @s.
+ */
 int _strlen(char *s)
 {
 int length;
