5136796f834bcc9309ec21d9c9efb9d475297c98
Idris Aminat <109235073+Aminat133@users.noreply.github.com>
2023-01-25 21:59:24 +0100

    Update 1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
-* 1-swap - swaps two variables value
-* @a - value being switched
-* @b - value being switched
+* swap_int - swaps two variables value
+* @a: value being switched
+* @b: value being switched
 *
 */
 void swap_int(int *a, int *b)
