21dcf94cca873e12600bdf841d17b28b53c911cf
Idris Aminat <109235073+Aminat133@users.noreply.github.com>
2023-01-28 22:28:38 +0100

    Update 3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,1 +1,13 @@
-#include 
+#include "main.h"
+
+/**
+*_puts - prnts a string follwed by a line
+*
+*@ *str: string to be printed
+* retun: 0
+*/
+
+void _puts(char *str)
+{
+print("%z/n", *str);
+}
