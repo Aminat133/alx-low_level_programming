77c17d3cdcc7b2faccabfba6480b5c756abd2cf8
Idris Aminat <109235073+Aminat133@users.noreply.github.com>
2023-01-28 22:47:23 +0100

    Update 3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -3,14 +3,14 @@
 /**
 *_puts - prnts a string follwed by a line
 *
-*@ *str: string to be printed
+*@*str: string to be printed
 * retun: 0
 */
 
 void _puts(char *str)
 {
- while (*str)
-   _putchar(*str++);
-   
- _putchar('\n');
+while (*str)
+_putchar(*str++);
+
+_putchar('\n');
 }
