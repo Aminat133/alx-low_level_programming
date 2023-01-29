b0be54a974617adcc76b8614ffa3eafaa41364cc
Idris Aminat <109235073+Aminat133@users.noreply.github.com>
2023-01-29 07:41:35 +0100

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,13 +10,12 @@ void rev_string(char *s)
 {
 int len = 0, index;
 char *m;
-char *b;
 
 while (s++)
 len++;
 
 for (index = len - 1; index >= 0; index--)
-m++ = --s;
+m++ = --*s;
 
 for (index = len - 1; index >= 0; index--)
 s = m++;
