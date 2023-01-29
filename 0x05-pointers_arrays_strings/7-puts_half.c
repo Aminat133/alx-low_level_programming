8b46a5f9e0265a07f3f4a0183f629d131e7fd6f1
Idris Aminat <109235073+Aminat133@users.noreply.github.com>
2023-01-29 23:37:26 +0100

    Update 7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,11 +8,17 @@
 
 void puts_half(char *str)
 {
-int len = 0, index = 0;
+int len = 0, index = 0, n;
 while (str[index++])
 len++;
 
-for (index = len/2 + 1 ; index <= len - 1; index++)
+  if ((len % 2) == 0)
+		n = len / 2;
+
+	else
+		n = (len + 1) / 2;
+
+for (index = n ; index <= len - 1; index++)
 _putchar(str[index]);
 
 _putchar('\n');
