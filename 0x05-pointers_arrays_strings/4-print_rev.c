64bd2b82c1da2be4697a9ce95f64c52f2dde57d1
Idris Aminat <109235073+Aminat133@users.noreply.github.com>
2023-01-29 01:17:05 +0100

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -12,11 +12,11 @@ void print_rev(char *s)
 {
 int len = 0, index;
 
-while (s[index++])
+while (*s[index++])
 len++;
 
 for (index = len - 1; index >= 0; index--)
-_putchar(s[index]);
+_putchar(*s[index]);
 
 _putchar('\n');
 }
