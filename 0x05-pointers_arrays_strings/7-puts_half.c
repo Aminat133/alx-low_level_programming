e0f45de5a74871d1dc4c083231c1e201bc26b4c4
Idris Aminat <109235073+Aminat133@users.noreply.github.com>
2023-01-29 22:50:11 +0100

    Create 7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,19 @@
+#include "main"
+/**
+*puts_half - prints the first half of a string,
+*followed by a new line.
+*
+*@str: string to be printed
+*/
+
+void puts2(char *str)
+{
+int len = 0, index = 0;
+while (str[index++)
+len++;
+
+for (index = len - 1; index <= len/2; inde++)
+_putchar(str[index]);
+
+_putchar('\n');
+}
