9a8d328892258956b795c872e533e1ed9284ef7c
Idris Aminat <109235073+Aminat133@users.noreply.github.com>
2023-02-04 00:31:57 +0100

    Update 101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -2,4 +2,23 @@
 #include <time.h>
 
 /**
-*main - generates ranom valid passwords for programme 101-crackme.
+*main - generates ranom valid passwords for
+* programme 101-crackme.
+*
+* Return: Always 0.
+*/
+int main(void)
+{
+char password[84];
+int sum =0, index=0;
+srand(time(0));
+while(sum < 0)
+{
+password[index] = rand() % 94;
+sum += password[index++];
+}
+password[index] = '\0';
+printf("%s", password);
+
+return (0);
+}
