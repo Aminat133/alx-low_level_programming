fddb93c0d9a7b69711a90c502d60899816b241c8
Idris Aminat <109235073+Aminat133@users.noreply.github.com>
2023-02-04 18:06:21 +0100

    Update main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,5 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
+/**
+ *
+ * *main.h - Header file containing declarations for all functions
+ *
+ * *       used in the directory.
+ *
+ * */
+
 
 char *_strcat(char *dest, char *src);
 #endif
