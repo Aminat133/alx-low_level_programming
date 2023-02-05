3170052621acf0b81e265be54cb9d726d1b6ad53
Idris Aminat <109235073+Aminat133@users.noreply.github.com>
2023-02-05 18:14:31 +0100

    Update 1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -15,13 +15,15 @@
 */
 char *_strncat(char *dest, char *src, int n)
 {
-  int index = 0, dest_len = 0;
-  
+int index = 0, dest_len = 0;
+
 while (dest[index++])
-  dest_len++;
-  
+dest_len++;
+
 for (index = 0; index <= n; index++)
 dest[dest_len++] = src[index];
 
-  return (dest);
+if (src < n)
+dest[dest_len++] = '\0';   
+return (dest);
 }
