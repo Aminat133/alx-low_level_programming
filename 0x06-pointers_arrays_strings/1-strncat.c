66415ecc47fa3e55d1dab13e5305f8e230cf535b
Idris Aminat <109235073+Aminat133@users.noreply.github.com>
2023-02-05 18:12:05 +0100

    Create 1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+* _strncat - concatenates two strings,
+*            using at most n bytes from src,
+*            src does not need to be
+*            null-terminated if it contains
+*            n or more bytes.
+*
+*@dest: initial string.
+*@src: string to be added.
+*@n: max number of chars that can be added.
+*
+*return: a pointer to the resulting string dest.
+*/
+char *_strncat(char *dest, char *src, int n)
+{
+  int index = 0, dest_len = 0;
+  
+while (dest[index++])
+  dest_len++;
+  
+for (index = 0; index <= n; index++)
+dest[dest_len++] = src[index];
+
+  return (dest);
+}
