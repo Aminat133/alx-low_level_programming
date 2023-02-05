d2c489c1844ee024902f63b14e5b5d2cc8b446d6
Idris Aminat <109235073+Aminat133@users.noreply.github.com>
2023-02-05 17:07:30 +0100

    Update trcat.c

--- 0x06-pointers_arrays_strings/trcat.c
+++ 0x06-pointers_arrays_strings/trcat.c
@@ -9,6 +9,20 @@
  * Return: A pointer to the destination string @dest.
  */
 
+/**
+ * char *_strcat - appends the src string
+ *  to the dest string, overwriting the
+ *  terminating null byte (\0) at the end 
+ *  of dest and then adds a terminating 
+ *  null byte.
+ *
+ * @dest: first string.
+ * @src: second string.
+ * 
+ * return: A pointer to the destination
+ * string @dest.
+ */
+
 char *_strcat(char *dest, char *src)
 {
 	int index = 0, dest_len = 0;
