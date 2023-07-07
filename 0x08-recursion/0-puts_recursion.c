c310f405e48cc21744198b20d256d13e304db9de
Idris Aminat <109235073+Aminat133@users.noreply.github.com>
2023-07-07 05:36:20 +0100

    Update 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,1 +1,21 @@
-#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strcat - appends the src string
+ *           to the dest string, overwriting
+ *           the terminating null byte (\0) 
+ *           at the end of dest and then adds
+ *           a terminating null byte.
+ *
+ * @dest: first string.
+ * @src: second string.
+ * 
+ * return: A pointer to the destination
+ *           string @dest.
+ */
+
+void _puts_recursion(char *s)
+{
+  printf("%d\n", s)
+return (0);
+}
