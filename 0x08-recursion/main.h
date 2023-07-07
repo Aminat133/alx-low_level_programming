26e6165ae9511a09ebd47abb42b3665d8b235fff
Idris Aminat <109235073+Aminat133@users.noreply.github.com>
2023-07-07 05:33:02 +0100

    Create main.h

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ *
+ * *main.h - Header file containing declarations for all functions
+ *
+ * *       used in the directory.
+ *
+ * */
+
+
+void _puts_recursion(char *s);
+#endif
