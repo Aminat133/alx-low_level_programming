95107b9b5bdf552c55551af36cfd963df4fe2be6
Idris Aminat <109235073+Aminat133@users.noreply.github.com>
2023-07-10 11:22:18 +0100

    Create 1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/*
+* main - A program that prints the number of arguements you passed into it
+* @argc: An argument counter
+* @argv: An argument values
+* return: Always 0 (success)*/
+
+int main(ine argc, char *argv[])
+{
+printf("%d\n", argc-1);
+return 0;
+}
