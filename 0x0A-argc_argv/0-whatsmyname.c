0842d9aaf82066ff1271abd0140af56efa14c4be
Idris Aminat <109235073+Aminat133@users.noreply.github.com>
2023-07-10 14:23:41 +0100

    updated file

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 
 /**
- * main - a program that prints its name, followed by a new line
+ * main - a program that prints its name, followed
+ * by a new line
  * @argc: arguement count
- * @argv: argument value, a strings that come after calling function
+ * @argv: argument value, a strings that come after
+ * calling function
  * Return: Always 0 (Success)
  **/
 
