f7945067e07c73d3b1621c0cd54f3df7a024d0f0
Idris Aminat <109235073+Aminat133@users.noreply.github.com>
2023-07-10 16:43:36 +0100

    Update 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -13,7 +13,7 @@ int main(int argc, char *argv[])
     int mult;
     if (argc != 3)
     {
-        printf("error");
+        printf("error\n");
         return (1);
     }
     mult = atoi(argv[1]) * atoi(argv[2]);
