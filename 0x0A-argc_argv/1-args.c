d81d14767d585a09b08b594d2b620cf60fb20da3
Idris Aminat <109235073+Aminat133@users.noreply.github.com>
2023-07-10 14:03:53 +0100

    Update 1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/*
-* main - A program that prints the number of arguements you passed into it
-* @argc: An argument counter
-* @argv: An argument values
-* return: Always 0 (success)
-**/
+/**
+ * main - A program that prints the number of arguements you passed into it
+ * @argc: An argument counter
+ * @argv: An argument values
+ * return: Always 0 (success)
+ **/
 
 int main(int argc, char *argv[])
 {
