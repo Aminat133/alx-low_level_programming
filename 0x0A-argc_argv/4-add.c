91ef22428f7210d6513fa8b4d41df084fe6b7149
Idris Aminat <109235073+Aminat133@users.noreply.github.com>
2023-07-11 15:55:47 +0100

    made updates

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -25,7 +25,6 @@ int main(int argc, char *argv[])
                 printf("Error\n");
                 return (1);
             }
-            
             sum += atoi(argv[i++]);
         }
         printf("%d\n", sum);
