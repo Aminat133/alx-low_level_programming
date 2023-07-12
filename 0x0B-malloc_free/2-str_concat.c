b613b539c0c4db87ef48941db4b89a0ee5d47f9d
Idris Aminat <109235073+Aminat133@users.noreply.github.com>
2023-07-12 01:48:04 +0100

    Update 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,6 +13,11 @@ char *str_concat(char *s1, char *s2)
 	char *a, *start;
 	int i = 0, j = 0, len1 = 0, len2 = 0;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
 	start = s1;
 	while (*s1++)
 		len1++;
