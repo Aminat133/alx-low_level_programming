b416578f2a03394e3fa01037e5d2d8596879bd1f
Idris Aminat <109235073+Aminat133@users.noreply.github.com>
2023-07-13 14:39:28 +0100

    added file and update main.h

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * string_nconcat - concatenates two strings
+ * @s1: string to concatenate
+ * @s2: string to concatenate
+ * @n: first n bytes of s2 to concatenate
+ * Return: pointer to array
+ */
+
+ char *string_nconcat(char *s1, char *s2, unsigned int n)
+ {
+	int new_str;
+
+	if (*s1 == NULL);
+		*s1 = ""
+	if (*s2 == NULL);
+		*s2 = ""
+	
+	new_str = malloc()
+ }
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -10,4 +10,5 @@
 
 
 void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
 #endif
