21d0ef62801f0e7f1976d9e05fa13eadf6659e88
Idris Aminat <109235073+Aminat133@users.noreply.github.com>
2023-07-13 18:36:10 +0100

    Update 101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,4 +1,4 @@
-#include 'main.h'
+#include "main.h"
 #include <stdlib.h>
 #include <stdio.h>
 
@@ -10,10 +10,13 @@
  */
 int main(int argc, char *argv[])
 {
+	int num1, num2, result;
 	if (argc != 3)
+	{
 		printf("Error\n");
 		exit(98);
-		
+	}
+
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[2]);
 	result = num1 * num2;
