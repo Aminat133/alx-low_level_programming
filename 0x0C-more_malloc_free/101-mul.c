65588bd8781dc514f76a159277c46d9d1e63edfd
Idris Aminat <109235073+Aminat133@users.noreply.github.com>
2023-07-13 18:33:29 +0100

    added files

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,29 @@
+#include 'main.h'
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - multiplies two positive numbers.
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 (sucess)
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+		printf("Error\n");
+		exit(98);
+		
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	result = num1 * num2;
+	printf("%d\n", result);
+	return (0);
+	/*
+	if (!(atoi(argv[1])))
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	*/
+}
