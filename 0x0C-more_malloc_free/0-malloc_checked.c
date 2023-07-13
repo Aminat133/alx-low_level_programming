9d55f015ff74594e20dcdbef95a9f0ee20f751db
Idris Aminat <109235073+Aminat133@users.noreply.github.com>
2023-07-13 13:03:18 +0100

    created directory

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+/**
+ *0-malloc_checked.c - a function that allocates memory using malloc.
+ * @b: size to allocate
+ * return: pointer to array
+ * */
+
+void *malloc_checked(unsigned int b)
+{
+	return (malloc(b));
+}
---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,13 @@
+#ifndef MIAN_H
+#define MAIN_H
+/**
+ *
+ * *main.h - Header file containing declarations for all functions
+ *
+ * *       used in the directory.
+ *
+ * */
+
+
+void *malloc_checked(unsigned int b);
+#endif
