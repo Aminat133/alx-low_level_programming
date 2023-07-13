2a0cfd8ab757fa697d36d5ef758d546aefb8168f
Idris Aminat <109235073+Aminat133@users.noreply.github.com>
2023-07-13 17:06:03 +0100

    created files

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _calloc - allocates memory for an array, using malloc.
+ * @nmemb: string to concatenate
+ * @size: string to concatenate
+ * Return: pointer to array
+ */
+
+ void *_calloc(unsigned int nmemb, unsigned int size)
+ {
+	int *call;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	call = malloc(nmemb * size);
+	if (call == NULL)
+		return (NULL);
+
+	for (i = 0; i < (nmemb * size); i++)
+		call[i] = 0;
+	return (call);
+	free(call);
+ }
