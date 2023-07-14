f24150933075fbbdfbc92a2d477c81869590cd7a
Idris Aminat <109235073+Aminat133@users.noreply.github.com>
2023-07-14 12:52:48 +0100

    Update 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -23,7 +23,7 @@ int main(int argc, char *argv[])
 		{
 			for (j = 0; argv[i][j]; j++)
 			{
-				if (!(atoi(argv[i][j])))
+				if (argv[i][j] < '0' || argv[i][j] > '9')
 				{
 					printf("Error\n");
 					return (1);
