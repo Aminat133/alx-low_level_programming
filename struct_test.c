ea1b66879c02dd5a42e71951a9048486c8b17c3c
Idris Aminat <109235073+Aminat133@users.noreply.github.com>
2023-07-18 00:42:31 +0100

    Create struct_test.c

---
+++ struct_test.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <stdio.h>
+struct user
+{
+  char *name;
+  char *email;
+  int age;
+};
+struct user *user(char *name, char* email,int age)
+{
+  struct user *user;
+  user = malloc(sieof(struct user));
+  if (user == NULL)
+    Return (NULL);
+  user->name = name;
+  user->email = email;
+  user->age = age;
+  return (user);
+}
+
+int main(void)
+{
+  struct user *user;
+  user = new_user("foo", "foo@foo.bar", 98);
+  if (user == NULL)
+    return (1);
+  printf("user %s created !\n", user->name);
+  printf("His email is: %s\n", user->email);
+  printf("And he is %d years old\n", user->age);
+  return (0);
+}
