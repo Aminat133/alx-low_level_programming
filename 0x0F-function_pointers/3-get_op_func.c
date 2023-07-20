ebe217d862f1027a28448cc0815570dd091accfc
Idris Aminat <109235073+Aminat133@users.noreply.github.com>
2023-07-20 13:31:12 +0100

    Update 3-get_op_func.c

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -7,22 +7,22 @@
  *to perform the operation asked by the user.
  *
  * @s: pointer to array to iterate
- * 
+ *
  * Return:  a pointer to the function that corresponds to the
  * operator given as a parameter.
  */
 
 int (*get_op_func(char *s))(int, int)
 {
-    op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
-    };
-    int i = 0;
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i = 0;
 
 	while (i < 5)
 	{
