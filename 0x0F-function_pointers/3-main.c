f7effa59cf5274e468d5865b70e735a4f751d90c
root <idris.oaminat@gmail.com>
2023-07-20 15:25:08 -0700

    update file

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -26,6 +26,6 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(99);
 	}
-	printf("%i", func(atoi(argv[1]), atoi(argv[3])));
+	printf("%d\n", func(atoi(argv[1]), atoi(argv[3])));
 	return (0);
 }
