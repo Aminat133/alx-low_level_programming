712ac25c05ec2c7d02cd4b7cdbbfbeb7c1222714
Idris Aminat <109235073+Aminat133@users.noreply.github.com>
2023-07-20 06:41:38 +0100

    Update 2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -6,22 +6,21 @@
  *
  * @array: pointer to array to iterate
  * @size: size of array
- * @action: pointer to function to execute 
+ * @cmp: pointer to function to execute
  * Return: nothing
  */
- int int_index(int *array, int size, int (*cmp)(int))
- {
+int int_index(int *array, int size, int (*cmp)(int))
+{
 	int i;
 
 
 	if (size <= 0)
 		return (-1);
-	
-	for (i= 0; i < size; i++)
+
+	for (i = 0; i < size; i++)
 	{
 		if (cmp(array[i]))
 			return (i);
 	}
 	return (-1);
- }
- 
+}
