1d913ce63a0abf6c43e5b3bf7fb4244c79496a7c
root <idris.oaminat@gmail.com>
2023-07-20 15:16:12 -0700

    update header file

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,16 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/**
- *
- * *main.h - Header file containing declarations for all functions
- *
- * *       used in the directory.
- *
- * */
-
-
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
+
 #endif
