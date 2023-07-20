90812141f9c3710377ccb56d87cd91da5cd4236d
Idris Aminat <109235073+Aminat133@users.noreply.github.com>
2023-07-20 06:18:01 +0100

    created files

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * array_iterator - executes a function given as a parameter on each element of an array.
+ *
+ * @array: pointer to array to iterate
+ * @f: size of array
+ * @action: pointer to function to execute 
+ * Return: nothing
+ */
