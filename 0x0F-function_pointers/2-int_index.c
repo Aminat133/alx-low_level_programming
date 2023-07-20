d94077974bb4a994b9ac61db1ceb2e9a99b786a0
Idris Aminat <109235073+Aminat133@users.noreply.github.com>
2023-07-20 06:21:51 +0100

    Update 2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -2,10 +2,14 @@
 #include "function_pointers.h"
 
 /**
- * array_iterator - executes a function given as a parameter on each element of an array.
+ * array_iterator - searches for an integer.
  *
  * @array: pointer to array to iterate
- * @f: size of array
+ * @size: size of array
  * @action: pointer to function to execute 
  * Return: nothing
  */
+ int int_index(int *array, int size, int (*cmp)(int))
+ {
+
+ }
