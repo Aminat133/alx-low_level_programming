5aa0d00d6c8602b982889a5f55b324a24beb76df
Idris Aminat <109235073+Aminat133@users.noreply.github.com>
2023-07-20 05:54:09 +0100

    rename main.h, update o-print_names

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <main.h>
+#include "function_pointers.h"
 /*
  * print_name - prints a name
  * @name: pointer to char to print
@@ -11,5 +11,5 @@ void print_name(char *name, void (*f)(char *))
 {
 	if (name == NULL || f == NULL)
 		return;
-	f(*name);
+	f(name);
 }
