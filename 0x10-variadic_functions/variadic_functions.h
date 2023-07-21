f1afe02e21bccdae2b24dd45f08ef0f9f0714669
Idris Aminat <109235073+Aminat133@users.noreply.github.com>
2023-07-21 23:08:19 +0100

    Create variadic_functions.h

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,9 @@
+#ifNdef VAR_FUNC
+#define VAR_FUNC
+
+int sum_them_all(const unsigned int n, ...)
+void print_numbers(const char *separator, const unsigned int n, ...)
+void print_strings(const char *separator, const unsigned int n, ...)
+void print_all(const char * const format, ...)
+
+#endif /*ifndeg VAR_FUNC*/
