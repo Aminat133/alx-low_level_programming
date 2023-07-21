0ac359f9048e889a3734de9974a8bea4516ba0bc
Idris Aminat <109235073+Aminat133@users.noreply.github.com>
2023-07-21 23:21:40 +0100

    Update variadic_functions.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,9 +1,9 @@
 #ifndef VAR_FUNC
 #define VAR_FUNC
 
-int sum_them_all(const unsigned int n, ...)
-void print_numbers(const char *separator, const unsigned int n, ...)
-void print_strings(const char *separator, const unsigned int n, ...)
-void print_all(const char * const format, ...)
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 
 #endif /*ifndeg VAR_FUNC*/
