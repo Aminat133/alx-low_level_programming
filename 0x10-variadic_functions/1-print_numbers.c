4269bc96fcbc7227e335dc1ff8a43aec3d5958dd
Idris Aminat <109235073+Aminat133@users.noreply.github.com>
2023-07-21 23:29:09 +0100

    Update 1-print_numbers.c

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,11 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * sum_them_all - returns the sum of all its parameters
+ * @n: - no. of other arguments, first and last required var.
+ * Return: int (sum of all its parameters)
+ */
+
+ void print_numbers(const char *separator, const unsigned int n, ...)
