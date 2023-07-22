7aabb556502e41fe66720b6532c803343581e37a
Idris Aminat <109235073+Aminat133@users.noreply.github.com>
2023-07-22 20:12:20 +0100

    Update 3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -34,7 +34,7 @@ void print_all(const char * const format, ...)
 				break;
 		case 's':
 				str = va_arg(ap, char*);
-			    if (str != NULL)
+				if (str != NULL)
 				{
 					printf("&s", str);
 					break;
