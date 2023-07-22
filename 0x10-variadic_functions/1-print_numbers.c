f4ac6929c099ecb5ff89e84a420413de9e6fa5ef
root <idris.oaminat@gmail.com>
2023-07-22 10:28:25 -0700

    update

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -22,7 +22,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 		if (separator == NULL)
 			continue;
-		if (i <= n - 1)
+		if (i < n - 1)
 			printf("%s", separator);
 	}
 	printf("\n");
