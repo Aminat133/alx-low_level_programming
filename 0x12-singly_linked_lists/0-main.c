9222eacb8fd2da81326cc45353a0954e88d7c70f
Idris Aminat <109235073+Aminat133@users.noreply.github.com>
2023-07-27 16:20:53 +0100

    Update 0-main.c

--- 0x12-singly_linked_lists/0-main.c
+++ 0x12-singly_linked_lists/0-main.c
@@ -14,7 +14,7 @@ int main(void)
 	list_t *new;
 	list_t hello = {"World", 5, NULL};
 	size_t n;
-	
+
 	head = &hello;
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
@@ -28,13 +28,13 @@ int main(void)
 	head = new;
 	n = print_list(head);
 	printf("-> %lu elements\n", n);
-	
+
 	printf("\n");
 	free(new->str);
 	new->str = NULL;
 	n = print_list(head);
 	printf("-> %lu elements\n", n);
-	
+
 	free(new);
 	return (0);
 }
