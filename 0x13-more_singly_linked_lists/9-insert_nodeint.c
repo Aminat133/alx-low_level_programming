4f179d32fc32503743842672545e56c773e042a3
Idris Aminat <109235073+Aminat133@users.noreply.github.com>
2023-08-01 01:53:03 +0100

    Update 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -41,7 +41,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		temp = temp->next;
 		i++;
 		if (temp = NULL)
-			return (NULL)
+			return (NULL);
 	}
 
 	new_node->n = n;
