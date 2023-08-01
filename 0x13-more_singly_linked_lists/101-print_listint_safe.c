d47b6b21e2730c06a5e1a02c86817d85cdc5cb13
Idris Aminat <109235073+Aminat133@users.noreply.github.com>
2023-08-01 02:36:42 +0100

    Update 101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -14,20 +14,11 @@ size_t print_listint_safe(const listint_t *head)
 	if (head == NULL)
 		exit(98);
 
-	while (one && two && two->next && head)
+	while (head)
 	{
-		one = one->next;
-		two = two->next->next;
-		if (one == two)
-		{
-			printf("-> [%p] %d\n", (void *)head, head->n);
-			exit(98);
-		}
-
 		printf("[%p] %d\n", (void *)head, head->n);
 		head = head->next;
 		nodes++;
 	}
-	head = NULL;
 	return (nodes);
 }
