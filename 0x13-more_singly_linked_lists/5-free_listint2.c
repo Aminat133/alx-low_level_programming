9dfbad468e7054e1cef89a0c97762a88aafb9b33
Idris Aminat <109235073+Aminat133@users.noreply.github.com>
2023-08-02 18:40:13 +0100

    Update 5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -14,9 +14,9 @@ void free_listint2(listint_t **head)
 
 	while (*head)
 	{
-		temp = *head;
-		(*head) = (*head)->next;
-		free(temp);
+		temp = *head->next;
+		free(*head);
+		*head = temp;
 	}
-	*head = NULL;
+	head = NULL;
 }
