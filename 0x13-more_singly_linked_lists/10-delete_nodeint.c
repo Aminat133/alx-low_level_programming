4a414e2c1fb5c889f4783279e79283cc0d732c71
Idris Aminat <109235073+Aminat133@users.noreply.github.com>
2023-08-02 19:59:18 +0100

    Update 10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -33,7 +33,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 			i++;
 		}
 		dnode = temp->next;
-		temp->next = dnode->next->next;
+		temp->next = dnode->next;
 		free(dnode);
 	}
 	return (1);
