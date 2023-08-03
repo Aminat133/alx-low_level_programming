efb2b27570ab244cf594839e863ebc6a997ba07c
Idris Aminat <109235073+Aminat133@users.noreply.github.com>
2023-08-03 01:00:51 +0100

    Update 102-free_listint_safe.c

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -3,43 +3,80 @@
 #include <stdlib.h>
 
 /**
+ * looped_count - counts no of unique nodes in a  list
+ * @head: A pointer to listint_t structure
+ * Return: 0 onnot looped list, no of unique nodes on success
+ */
+size_t looped_count(listint_t *head)
+{
+	const listint_t *tortoise, *hare;
+	size_t nodes = 1;
+
+	if (head == NULL || head->next == NULL)
+		return (0);
+
+	hare = head->next->next;
+	tortoise = head->next;
+
+	while(hare)
+	{
+		if (tortoise == hare)
+		{
+			tortoise = head;
+			while (tortoise != hare)
+			{
+				nodes++;
+				tortoise = tortoise->next;
+				hare = hare->next;
+			}
+
+			tortoise = tortoise->next;
+			while (tortoise != hare)
+			{
+				nodes++;
+				tortoise = tortoise->next;
+			}
+
+			return (nodes);
+		}
+		tortoise =tortoise->next;
+		hare = hare->next->next;
+	}
+	return (0);
+}
+
+/**
  * free_listint_safe - A function that frees a list
  * @h: A pointer listint_t structure
  * Return: The size of the list that was free'd
  */
 size_t free_listint_safe(listint_t **h)
 {
-	size_t counter = 0;
+	size_t nodes, i = 0;
 	listint_t *temp;
 
-	temp = *h;
-	while (temp)
+	nodes = looped_count(*h);
+
+	if (nodes == 0)
 	{
-		temp = *h;
-		temp = temp->next;
-		free_list(temp);
-		counter++;
+		while (*h)
+		{
+			temp = *h;
+			(*h) = (*h)->next;
+			free(temp);
+		}
 	}
-	*h = NULL;
-
-	return (counter);
-}
-
-/**
- * free_list - A function that frees a listint_t recursively
- * @head: A pointer to the listint_t structure
- * Return: Nothing
- */
-void free_list(listint_t *head)
-{
-	listint_t *temp;
-
-	if (head)
+	else
 	{
-		temp = head;
-		temp = temp->next;
-		free(temp);
-		free_list(temp);
+		for (; i <= nodes; i++)
+		{
+			temp = *h;
+			(*h) = (*h)->next;
+			free(temp);
+		}
+		*h = NULL;
 	}
-	free(head);
+	h = NULL;
+
+	return (nodes);
 }
