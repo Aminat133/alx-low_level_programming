2c06f9ea1edf2fee4661a6e6fefdd332b9cab476
Idris Aminat <109235073+Aminat133@users.noreply.github.com>
2023-09-12 04:53:46 +0100

    Create 5-free_dog.c

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ *free_dog-  a function that frees dogs.
+ * @d: structure to free
+ */
+
+ void free_dog(dog_t *d)
+ {
+	if (d == NULL)
+		return (NULL);
+	free(d->name);
+	free(d->owner);
+	free(d)
+ }
+ 
