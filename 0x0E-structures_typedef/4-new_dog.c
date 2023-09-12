a069ce266a9fd345ed64e1806073da99339ee861
Idris Aminat <109235073+Aminat133@users.noreply.github.com>
2023-09-12 06:01:04 +0100

    Update 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -49,11 +49,12 @@ char *_strcpy(char *dest, char *src)
 
  dog_t *new_dog(char *name, float age, char *owner)
  {
+	dog_t (*p);
 
 	if (name == NULL || age < 0 || owner == NULL)
 		return (NULL);
 
-	dog_t *p = malloc(sizeof(dog_t));
+	p = malloc(sizeof(dog_t));
 	if (p == NULL)
 		return (NULL);
 
