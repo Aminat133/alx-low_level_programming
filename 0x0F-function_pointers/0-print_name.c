aa8a985279d1254dd8b4551de015abd8a5c2b8ad
Idris Aminat <109235073+Aminat133@users.noreply.github.com>
2023-09-13 21:25:41 +0100

    creat files

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "function_pointers.h"
+#include <stdlib.h>
+
+/**
+ * print_name - prints a name
+ *
+ * @name: pointer to char to print
+ * @f: pointer to function
+ * Return: nothing
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+		f(name);
+}
---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * array_iterator - executes a function given
+ *as a parameter on each element of an array.
+ *
+ * @array: pointer to array to iterate
+ * @size: size of array
+ * @action: pointer to function to execute
+ * Return: nothing
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+		if (action == NULL || array == NULL)
+		return;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
