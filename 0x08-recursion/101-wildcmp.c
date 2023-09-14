706143c4a0631cd56aba53693c2f7b2834080472
Idris Aminat <109235073+Aminat133@users.noreply.github.com>
2023-09-15 00:51:00 +0100

    Update 101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -9,6 +9,8 @@ int wildcmp(char *s1, char *s2)
 {
 	if (!*s1 && !*s2)
 		return (1);
+	if (!*s1)
+		return (0);
 	if (*s1 == *s2)
 		return (wildcmp(s1 + 1, s2 + 1));
 	if (*s2 == '*' && (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2)))
