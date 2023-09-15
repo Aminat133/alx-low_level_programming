6eecf13a879c2c174e87b0669061d342574b07d8
Aminat133 <idris.oaminat@gmail.com>
2023-09-14 17:04:20 -0700

    update

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -13,7 +13,7 @@ int wildcmp(char *s1, char *s2)
 	if (*s1 == *s2)
 		return (wildcmp(s1 + 1, s2 + 1));
 	
-	if (*s2 == '*' && (wildcmp(s1, s2 + 1) || (*s1 != "\n" && wildcmp(s1 + 1, s2))))
+	if (*s2 == '*' && (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2)))
 		return (1);
 	if (*s2 == '*' && *(s1 + 1) && *s2)
 		return (0);
