590ee731b70933258b8d7e9124622bb5c8d99b85
Idris Aminat <109235073+Aminat133@users.noreply.github.com>
2023-09-29 05:55:43 +0100

    Create 3-set_bit.c

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+
+/**
+ * set_bit - sets a bit  hmm at given index to 1
+ * @n: The number to set bit in
+ * @index: The index to set bit at
+ * Return: 1 if it worked, or -1 on error
+ */
+
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int maxi = 0x01;
+
+	maxi <<= index;
+	if (maxi == 0)
+		return (-1);
+	*n |= maxi;
+	return (1);
+}
