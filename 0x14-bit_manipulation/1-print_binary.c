7613d9c11134642e3607b00d69b5128646fc456d
Idris Aminat <109235073+Aminat133@users.noreply.github.com>
2023-09-29 05:52:49 +0100

    Create 1-print_binary.c

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+
+/**
+ * print_binary - prints a binary number without % or /
+ * @n: The number to be printed.
+ * Return: Nothing.
+ */
+
+void print_binary(unsigned long int n)
+{
+	unsigned int flag = 0, maxi = 32768; /* 1000 0000 0000 0000 */
+
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+  
+	while (maxi)
+	{
+		if (flag == 1 && (n & maxi) == 0)
+			_putchar('0');
+      
+		else if ((n & maxi) != 0)
+		{
+			_putchar('1');
+			flag = 1;
+		}
+    
+		maxi >>= 1;
+	}
+}
