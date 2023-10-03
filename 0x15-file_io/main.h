2f7c5d8227cbda5cc2363feb5cb3ed04ad7af7c5
Idris Aminat <109235073+Aminat133@users.noreply.github.com>
2023-10-03 06:19:10 +0100

    Update main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <stddef.h>
-size_t read_textfile(const char *filename, size_t letters);
+ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 #endif /* MAIN_H */
